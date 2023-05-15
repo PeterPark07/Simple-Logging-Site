434dcf982632d7489ef2059c9780eb78940f3db5
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-15 22:44:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/logs', methods=['POST'])
+def save_logs():
+    log_data = request.get_data(as_text=True)
+
+    with open('logs.txt', 'a') as f:
+        f.write(log_data + '\n')
+
+    return 'Log saved successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True)

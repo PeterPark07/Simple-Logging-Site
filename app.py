5a841ccb6e577a532e2c65c401ba2f64799da9c0
PeterPark07 <128113413+PeterPark07@users.noreply.github.com>
2023-05-16 01:23:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,53 @@
 from flask import Flask, request, render_template
-import os
 
 app = Flask(__name__)
 
-@app.route('/logs', methods=['POST'])
-def save_logs():
-    log_data = request.get_data(as_text=True)
+# File names
+LOGS_FILE = 'logs.txt'
+DATA_FILE = 'data.txt'
 
-    with open('logs.txt', 'a') as f:
-        f.write(log_data + '\n')
+def write_to_file(filename, data):
+    # Append data to the specified file.
+    with open(filename, 'a') as f:
+        f.write(data + '\n')
 
-    return 'Log saved successfully'
+def read_file(filename):
+    # Read the contents of the specified file.
+    with open(filename, 'r') as f:
+        return f.read()
 
-@app.route('/logs', methods=['GET'])
-def get_logs():
-    if not os.path.exists('logs.txt'):
-        return 'No logs available'
+@app.route('/logs', methods=['POST', 'GET'])
+def logs():
+    # Handle POST and GET requests for logs.
+    if request.method == 'POST':
+        # Write log data to logs file
+        log_data = request.get_data(as_text=True)
+        write_to_file(LOGS_FILE, log_data)
+        return 'Saved successfully'
+    else:
+        # Read logs file and return its contents
+        logs = read_file(LOGS_FILE)
+        return logs
 
-    with open('logs.txt', 'r') as f:
-        logs = f.read()
-
-    return logs
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    # Handle POST and GET requests for data.
+    if request.method == 'POST':
+        # Write log data to data file
+        log_data = request.get_data(as_text=True)
+        write_to_file(DATA_FILE, log_data)
+        return 'Saved successfully'
+    else:
+        # Read data file and return its contents
+        data = read_file(DATA_FILE)
+        return data
 
 @app.route('/show', methods=['GET'])
-def home():
-    if not os.path.exists('logs.txt'):
-        # Create the file if it doesn't exist
-        with open('logs.txt', 'w') as f:
-            pass
-
-    with open('logs.txt', 'r') as f:
-        logs = f.read()
-
-    return render_template('home.html', logs=logs)
+def show():
+    # Render the home page and display contents of logs and data files.
+    logs = read_file(LOGS_FILE)
+    data = read_file(DATA_FILE)
+    return render_template('home.html', logs=logs, data=data)
 
 if __name__ == '__main__':
-    # Use Gunicorn as the production server
-    from gunicorn.app.wsgiapp import run as run_gunicorn
-    run_gunicorn(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
